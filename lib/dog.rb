f9f7aecdb455e5d3f7a4f7afa403708ba12f0c47
andy.chadha2 <andy.chadha2@gmail.com>
2018-10-09 21:31:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,15 @@ class Dog
     @name = name
   end
   
+  def name=(new_name)
+    @name = new_name
+  end
   
+  def breed_initial(breed)
+    @breed = breed
+  end
+  
+  def breed_new(new breed)
+    @breed = new_breed
+end
 end
